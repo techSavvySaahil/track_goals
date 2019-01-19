d5d2a53f97f1213594eda1b7eaea820e6240e601
techSavvySaahil <saahil.madaan@gmail.com>
2019-01-19 11:15:12 +0530

    Updates

---
+++ precache-manifest.8999788fbf00b4e252111ca8aa978051.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7c41d7fd0b21a8b1dfb71a86f74f8d8",
+    "url": "/techSavvySaahil/track_goals/static/media/Manager.a7c41d7f.jpg"
+  },
+  {
+    "revision": "191c15276ea847301619d81a4c49f7a7",
+    "url": "/techSavvySaahil/track_goals/static/media/Agent4.191c1527.jpg"
+  },
+  {
+    "revision": "5c87ddae3c13201169f416900deba4cb",
+    "url": "/techSavvySaahil/track_goals/static/media/Agent3.5c87ddae.jpg"
+  },
+  {
+    "revision": "50f2a075a2c6e6faf1d2f665d3eaa6ce",
+    "url": "/techSavvySaahil/track_goals/static/media/Agent2.50f2a075.jpg"
+  },
+  {
+    "revision": "93ca6e295d494e2c5cf26a8a7598d307",
+    "url": "/techSavvySaahil/track_goals/static/media/Agent1.93ca6e29.png"
+  },
+  {
+    "revision": "39af59872fbcee89f3a1",
+    "url": "/techSavvySaahil/track_goals/static/js/runtime~main.39af5987.js"
+  },
+  {
+    "revision": "89b8003efa132fa752dd",
+    "url": "/techSavvySaahil/track_goals/static/js/main.89b8003e.chunk.js"
+  },
+  {
+    "revision": "bae70bb8c9e5e22b97d2",
+    "url": "/techSavvySaahil/track_goals/static/js/1.bae70bb8.chunk.js"
+  },
+  {
+    "revision": "89b8003efa132fa752dd",
+    "url": "/techSavvySaahil/track_goals/static/css/main.10ca1384.chunk.css"
+  },
+  {
+    "revision": "bcc0ea768726c37512af7ccce3351818",
+    "url": "/techSavvySaahil/track_goals/index.html"
+  }
+];
